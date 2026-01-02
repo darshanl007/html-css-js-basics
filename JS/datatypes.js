31ce3fec67aec36d4581d62bd13d4895bcaf8783
Darshan L <darshanl1254@gmail.com>
2026-01-02 16:01:10 +0530

    datatypes in js - primitive

---
+++ JS/datatypes.js
@@ -0,0 +1,37 @@
+// Primitive Datatypes : Number, String, Boolean, Undefined, Null, BigInt, Symbol.
+
+//Number
+let price = 500;
+console.log(price);
+console.log(typeof(price));
+
+//String 
+let Name = "Darshan"
+console.log(Name);
+console.log(typeof(Name));
+
+//Boolean
+let isFollow = true;
+console.log(isFollow);
+console.log(typeof(isFollow));
+
+//Undefined
+let  x;
+console.log(x);
+console.log(typeof(x));
+
+//Null
+let y = null;
+console.log(y);
+console.log(typeof(y));
+
+//BigInt
+let num = BigInt(123);
+console.log(num);
+console.log(typeof(num));
+
+//Symbol
+let z = Symbol("Hello World!");
+console.log(z);
+console.log(typeof(z));
+
