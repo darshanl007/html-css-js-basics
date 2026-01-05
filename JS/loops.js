78785a6418f8db1f32a21dc9cac16197c32634f9
Darshan L <darshanl1254@gmail.com>
2026-01-05 11:07:22 +0530

    loops in javascript (for loop)

---
+++ JS/loops.js
@@ -0,0 +1,23 @@
+//Loops
+
+//1. for loop
+for(let i = 1;i <= 5; i++){
+    console.log("Hello Javascript");
+}
+
+//print number from 1 to 10
+console.log("Numbers from 1 to 10");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log("Loop Ends");
+
+//print even number in 1 to 20
+console.log("Even numbers")
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
