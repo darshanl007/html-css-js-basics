3c435ad3697ffe6d00115299f77b7b48e5ddad55
Darshan L <darshanl1254@gmail.com>
2026-01-09 15:38:16 +0530

    dom content properties

---
+++ JS/DOM/dom_properties.js
@@ -0,0 +1,17 @@
+// Selecting elements
+let title = document.getElementById("title");
+let container = document.getElementById("container");
+let para = document.getElementById("para");
+let input = document.getElementById("inputBox");
+let checkbox = document.getElementById("checkBox");
+let button = document.getElementById("btn");
+
+// Content Properties
+title.textContent = "Updated using textContent";
+para.innerText = "Changed using innerText";
+container.innerHTML = container.innerHTML + "<p>Added using innerHTML</p>";
+console.log(title.tagName);
+console.log(container.tagName);
+console.log(para.tagName);
+
+console.log("-------------------------");
