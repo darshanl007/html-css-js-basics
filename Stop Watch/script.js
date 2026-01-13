df802f93d3cb542ccc3ab4afcf10be095e35403d
Darshan L <darshanl1254@gmail.com>
2026-01-13 17:54:10 +0530

    stop watch using html, css, javascript

---
+++ Stop Watch/script.js
@@ -0,0 +1,64 @@
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let timer;
+let isRunning = false;
+
+// Update Display
+function updateDisplay() {
+    let display = document.getElementById("display");
+    display.innerText = `${pad(minutes)} : ${pad(seconds)} : ${pad(milliseconds)}`;
+}
+
+function pad(num){
+    return num < 10 ? "0" + num : num;
+}
+
+// Update time values
+function updateTime(){
+    milliseconds++;
+
+    if(milliseconds === 100){
+        milliseconds = 0;
+        seconds++;
+    } 
+
+    if(seconds === 60){
+        seconds = 0;
+        minutes++;
+    }
+
+    updateDisplay();
+}
+
+// Start
+function start(){
+    if(!isRunning){
+        isRunning = true;
+        timer = setInterval(updateTime,10);
+    }
+}
+
+// Stop 
+function stop(){
+    clearInterval(timer);
+    isRunning = false;
+}
+
+// Reset
+function reset() {
+    clearInterval(timer);
+    isRunning = false;
+    minutes = seconds = milliseconds = 0;
+    updateDisplay();
+}
+
+// DOM Selection
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+
+// Add event listeners
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
